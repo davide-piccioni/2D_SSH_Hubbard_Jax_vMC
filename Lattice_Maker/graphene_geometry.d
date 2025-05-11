85c520d48ce8fcbffb2c26aeadeb8e967b7e24cb
dpiccion <dpiccion@sissa.it>
2025-05-11 17:48:29 +0200

    changed folder positions

--- Lattice_Maker/graphene_geometry.d
+++
@@ -1,5 +0,0 @@
-'' 1. '' 1. 60. ! lunghezza a_1, lunghezza a_2, angolo tra i due
-2                     ! numero di siti per cella
-'' 0. '' 0.
-'' 0.33333333333333333333 '' 0.33333333333333333333
-3 0 0 3             ! n_11, n_12, n_21, n_22 per T_1 e T_2
--- Lattice_Maker/triangular_geometry.d
+++
@@ -1,4 +0,0 @@
-'' 1. '' 1. 60. ! lunghezza a_1, lunghezza a_2, angolo tra i due
-1                     ! numero di siti per cella
-'' 0. '' 0.
-10 0 0 6             ! n_11, n_12, n_21, n_22 per T_1 e T_2
